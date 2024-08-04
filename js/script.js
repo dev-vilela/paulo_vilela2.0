4a61be6854e7e300fe12008c1dc20c8fc23e04d8
dev-vilela <paulov4@hotmail.com>
2024-08-04 16:07:23 -0300

    em andamento

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
 
-// BOTÃO VEJA MAIS - PROJETOS
-const ButtonSeeMore = document.getElementById('see-more')
-const hideDiv = document.getElementById('hide-content')
-
-ButtonSeeMore.addEventListener('click', () => {
-    hideDiv.classList.toggle('show');
-    if (hideDiv.classList.contains('show')) {
-        ButtonSeeMore.innerHTML = 'Ver menos';
-        ButtonSeeMore.style.backgroundColor = '#D62828';
-    } else {
-        ButtonSeeMore.innerHTML = 'Ver mais';
-        ButtonSeeMore.style.backgroundColor = '#FCBF49';
-    }
-});
-
 
 // MENU HAMBURGUER
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburguer = document.querySelector('.hamburguer');
+    const hamburguer = document.querySelector('.menu-card');
     const navMenu = document.querySelector('.nav-menu');
 
     hamburguer.addEventListener('click', function () {
@@ -27,26 +12,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 const lamp = document.querySelector('.lamp');
-const containerLamp = document.querySelector('.cta');
-const textLamps = document.querySelectorAll('.txt-cta');
+
+
 const moon = document.querySelector('.switch');
 const moonImage = document.getElementById('moonImage');
 
 
-//LAMPADA - CTA
-function toggleLamp() {
-    containerLamp.classList.toggle('active');
-    textLamps.forEach(textLamp => {
-        textLamp.classList.toggle('active');
-    });
 
-    if (containerLamp.classList.contains('active')) {
-        lamp.src = './assets/icons/dark-lamp.svg';
-    } else {
-        lamp.src = './assets/icons/light_lamp.svg'; 
-    }
-}
-lamp.addEventListener('click', toggleLamp);
 
 //DARK MODE
 moon.addEventListener('click', function() {
